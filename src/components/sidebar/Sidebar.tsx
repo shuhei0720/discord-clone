fc5e335dd805a9131ec4a0f23b8395a7cc1f010b
shuhei0720 <shuhei_0720@icloud.com>
2025-06-16 00:20:37 +0900

    チャネルのヘッダーを作成

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Sidebar.scss";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import AddIcon from "@mui/icons-material/Add";
 
 const Sidebar = () => {
   return (
@@ -19,6 +20,17 @@ const Sidebar = () => {
         <div className="sidebarTop">
           <h3>Discord</h3>
           <ExpandMoreIcon />
+        </div>
+
+        {/* sidebarChannels */}
+        <div className="sidebarChannels">
+          <div className="sidebarChannelsHeader">
+            <div className="sidebarHeader">
+              <ExpandMoreIcon />
+              <h4>プログラミングチャンネル</h4>
+            </div>
+            <AddIcon className="sidebarAddIcon" />
+          </div>
         </div>
       </div>
     </div>
