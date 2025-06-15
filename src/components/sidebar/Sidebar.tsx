5ae969f563ec7d34e8d56f18c3d44a65c35bd163
shuhei0720 <shuhei_0720@icloud.com>
2025-06-15 21:49:07 +0900

    サイドバーのHTMLとCSSを記述

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -5,9 +5,20 @@ const Sidebar = () => {
   return (
     <div className="sidebar">
       {/* sidebarLeft */}
-      <div className="sidebarLeft"></div>
+      <div className="sidebarLeft">
+        <div className="serverIcon">
+          <img src="./logo192.png" alt="" />
+        </div>
+        <div className="serverIcon">
+          <img src="./logo192.png" alt="" />
+        </div>
+      </div>
       {/* sidebarRight */}
-      <div className="sidebarRight"></div>
+      <div className="sidebarRight">
+        <div className="sidebarTop">
+          <h3>Discord</h3>
+        </div>
+      </div>
     </div>
   );
 };
