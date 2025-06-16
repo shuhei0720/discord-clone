a04f0cdca791551fdb488ebe74ad73b1276e01e2
shuhei0720 <shuhei_0720@icloud.com>
2025-06-16 09:11:05 +0900

    サイドバーフッターのHTMLを作成

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -3,6 +3,10 @@ import "./Sidebar.scss";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AddIcon from "@mui/icons-material/Add";
 import SidebarChannel from "./SidebarChannel";
+import MicIcon from "@mui/icons-material/Mic";
+import HeadphonesIcon from "@mui/icons-material/Headphones";
+import SettingsIcon from "@mui/icons-material/Settings";
+import { Mic } from "@mui/icons-material";
 
 const Sidebar = () => {
   return (
@@ -38,6 +42,22 @@ const Sidebar = () => {
             <SidebarChannel />
             <SidebarChannel />
             <SidebarChannel />
+          </div>
+
+          <div className="sidebarFooter">
+            <div className="sidebarAccount">
+              <img src="./icon.png" alt="" />
+              <div className="accountName">
+                <h4>shuhei</h4>
+                <span>#8162</span>
+              </div>
+            </div>
+
+            <div className="sidebarVoice">
+              <MicIcon />
+              <HeadphonesIcon />
+              <SettingsIcon />
+            </div>
           </div>
         </div>
       </div>
